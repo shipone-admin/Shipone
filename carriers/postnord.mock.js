fe02312fad0a30f3e4052f8d398bb7f466bf6370
shipone-admin <barbarossa799@gmail.com>
2026-02-23 06:44:54 +0100

    Update postnord.mock.js

--- carriers/postnord.mock.js
+++ carriers/postnord.mock.js
@@ -1,29 +1,38 @@
+// carriers/postnord.mock.js
+
 // Mock version of PostNord rates (used until API is live)
 
-function getPostNordRates(order) {
+async function getRates(order) {
+  console.log("📡 PostNord mock rates requested");
+
   return [
     {
+      carrier: "postnord",
       id: "PN_SERVICE_POINT",
       name: "Service Point",
       price: 59,
       eta_days: 2,
-      co2: 1.2,
+      co2: 1.2
     },
     {
+      carrier: "postnord",
       id: "PN_HOME",
       name: "Home Delivery",
       price: 79,
       eta_days: 2,
-      co2: 1.6,
+      co2: 1.6
     },
     {
+      carrier: "postnord",
       id: "PN_EXPRESS",
       name: "Express",
       price: 109,
       eta_days: 1,
-      co2: 2.4,
-    },
+      co2: 2.4
+    }
   ];
 }
 
-module.exports = { getPostNordRates };
+module.exports = {
+  getRates
+};
